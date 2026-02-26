08cd8541d5090fb83b924ca9dbca7c7257057d90
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-02-26 06:10:06 +0500

    Add DateFormat to handle the complex date issue to make it modern and simple time duration of the post

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -4,6 +4,6 @@ export { default as PostCard } from "./PostCard";
 export { default as LeftSidebar } from "./LeftSidebar";
 export { default as TopBar } from "./TopBar";
 export { default as FormUploader } from "./FormUploader";
-
+export { default as PostStats } from "./PostStats";
 
 
